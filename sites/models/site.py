4babd709dbeebbb422dc3189e89bbffe52a58a5d
Juan Tabares <juan.ctt@live.com>
2018-03-27 00:13:19 +0900

    Add sites model

--- sites/models/site.py
+++ sites/models/site.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.db import models as md
 
-from django.db import models
 
-# Create your models here.
+class Site(md.Model):
+    name = md.CharField(max_length=200)
+    created_at = md.DateTimeField(auto_now_add=True)
+    updated_at = md.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        indexes = [
+            md.Index(fields=['name'])
+        ]
+
+    @classmethod
+    def get_sites(cls, page=1, limit=20):
+        q = cls.objects.order_by('-created_at')
+        total = q.count()
+        return q[(page - 1) * limit:limit], total
--- sites/views/index.py
+++ sites/views/index.py
@@ -2,11 +2,16 @@
 from __future__ import unicode_literals
 from django.views import View
 from django.shortcuts import render
+from sites.models import Site
 
 
 class IndexView(View):
 
     def get(self, request):
+        sites, total = Site.get_sites()
+
         return render(request, 'sites/index.html', {
-            'title': 'Sites'
+            'title': 'Sites',
+            'total': total,
+            'sites': sites
         })
